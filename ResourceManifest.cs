4a6406c0cafd1d4c1fabfdb068ca07611fbb258b
Albert Hajdu <albert.hajdu@lombiq.com>
2016-02-09 12:11:51 +0100

    Updating angular2 source to beta 3. Adding 3rd party dependencies.

---
+++ ResourceManifest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Orchard.UI.Resources;
+
+namespace Orchard.Angular2
+{
+    public class ResourceManifest : IResourceManifestProvider
+    {
+        public void BuildManifests(ResourceManifestBuilder builder)
+        {
+            var manifest = builder.Add();
+
+            manifest.DefineScript("Es6Shim").SetUrl("es6-shim/es6-shim.min.js");
+            manifest.DefineScript("SystemPolyfills").SetUrl("systemjs/dist/system-polyfills.js");
+            manifest.DefineScript("Angular2Polyfills").SetUrl("angular2/bundles/angular2-polyfills.min.js");
+            manifest.DefineScript("SystemJs").SetUrl("systemjs/dist/system.src.js");
+            manifest.DefineScript("RxJs").SetUrl("rxjs/bundles/Rx.min.js");
+            manifest.DefineScript("angular2").SetUrl("angular2/bundles/angular2.dev.js").SetDependencies("Es6Shim", "SystemPolyfills", "Angular2Polyfills", "SystemJs", "RxJs");
+        }
+    }
+}
